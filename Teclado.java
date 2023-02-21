26a6b11d68347b21fe3f22269aa6e11f11a79bb1
Marckytos <marquillos.bros3451@gmail.com>
2023-02-21 12:33:37 -0600

    Teclado.java

--- Teclado.java
+++ Teclado.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class Teclado {
     public static void main(String args []){
-        String entrada = "hola";
+        Scanner teclado = new Scanner(System.in);
+        String entrada = teclado.nextLine();
+        System.out.println("Capturaste " + entrada);
+
     }
     
 }
